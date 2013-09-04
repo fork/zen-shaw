0aaa195ebc80ca6d9ce0192b5796d214942b1a5a
Alexander Dobbert <adobbert@fork.de>
2013-09-04 13:16:06 +0200

    Set up basic tree traversal function.

--- src/Zen.js
+++ src/Zen.js
@@ -1,7 +1,8 @@
 // // create a new parser
-var parser = emmet.require('abbreviationParser');
-var emmetString = "ul>li{List Item $}*3>a";
-var parsed = parser.parse(emmetString);
+var parser = emmet.require('abbreviationParser'),
+output,
+emmetString, parsed;
+
 // the parser returns an object
 console.log(parsed);
 // 
@@ -20,4 +21,26 @@ function Zen() {
 	var element = document.createElement('div');
 	element.classList.add('a-class');
 	return element;
+}
+
+function run() {
+	output = document.getElementById("output"),
+	output.innerHTML="--- Go! ---<br/>"
+	var input = document.getElementById("emmet-string");
+	emmetString = input.value;
+	parsed = parser.parse(emmetString);
+	DOMBuilder.traverseTree(parsed)
+}
+
+var DOMBuilder = {}
+
+DOMBuilder.traverseTree = function(base) {
+	for (var node=0; node < base.children.length; node++) {
+			DOMBuilder.output(base.children[node]._name);
+			DOMBuilder.traverseTree(base.children[node]);
+	}
+}
+
+DOMBuilder.output = function(node) {
+	output.innerHTML+=node +"<br/>";
 }
